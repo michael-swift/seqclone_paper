e39a840eadffd33fc427f15221e28df057477e74
Michael <swiftmichael26@gmail.com>
2022-05-09 08:13:18 -0700

    made svg2pdf have better fidelity conversion, still an issue with text from inkscape looking weird...

--- scripts/svg2pdf.sh
+++ scripts/svg2pdf.sh
@@ -1,3 +1,3 @@
 # provide the directory with the pdfs you'd like to convert as the 1st argument
 cd $1
-for f in ./*.svg ; do convert "$f" "${f%}.pdf" ; done
+for f in ./*.svg ; do convert -density 343 "$f" "${f%}.pdf" ; done
