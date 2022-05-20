a17f582f732fc9bf7572d6dda0eb9efee80f5998
Michael <swiftmichael26@gmail.com>
2022-05-20 09:10:02 -0700

    modified svg2pdf to do better resolution, added volcano plot to figure 4

--- scripts/svg2pdf.sh
+++ scripts/svg2pdf.sh
@@ -1,6 +1,6 @@
 # provide the directory with the pdfs you'd like to convert as the 1st argument
 cd $1
-for f in ./*.svg ; do convert -density 500 "$f" "${f%}.pdf" ; done
+for f in ./*.svg ; do convert -density 800 "$f" "${f%}.pdf" ; done
 
 rename 's/.svg.pdf/.pdf/' *.pdf
 
