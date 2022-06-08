efbf658eb7281a874973a62076c66a00f84b58ea
Michael <swiftmichael26@gmail.com>
2022-06-07 22:52:59 -0700

    added in vivo clonal genes plot and first pass at S4

--- README.md
+++ README.md
@@ -4,8 +4,4 @@
 
 https://docs.google.com/document/d/1gJ7tvSlHpDyeUUQXchSil8Ps4px_omjntSjhFLTF5fc/edit?usp=sharing
 
-### old
-https://docs.google.com/document/d/1RjEdhlqGjTdjggkkLY6o4V_DNnLfwv4sMNTSYCO07zM/edit?usp=sharing
-
-
 
--- scripts/svg2pdf.sh
+++ scripts/svg2pdf.sh
@@ -1,4 +1,4 @@
-# provide the directory with the pdfs you'd like to convert as the 1st argument
+# provide the directory with the svg's you'd like to convert as the 1st argument
 cd $1
 for f in ./*.svg ; do convert -density 800 "$f" "${f%}.pdf" ; done
 
